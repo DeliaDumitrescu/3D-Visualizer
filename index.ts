dfdb5b4a3c110086405764dfcebdcbbe0e59ca58
antonioganea <antonio.ganea@yahoo.com>
2021-03-26 23:19:48 +0200

    Added static file serving

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = 80;
+app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Express + TypeScript Server! It works!'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
