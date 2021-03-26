a0a86ee408a74e1b4fd2856969657d117f1729d7
antonioganea <antonio.ganea@yahoo.com>
2021-03-26 23:21:54 +0200

    Added ejs to package.json

--- index.ts
+++ index.ts
@@ -3,7 +3,15 @@ import express from 'express';
 const app = express();
 const PORT = 80;
 app.use(express.static('public'))
-app.get('/', (req, res) => res.send('Express + TypeScript Server! It works!'));
+app.get('/', (req, res) =>{
+  
+  let ejs = require('ejs');
+  let people = ['geddy', 'neil', 'alex'];
+  let html = ejs.render('<%= people.join(", "); %>', {people: people});
+
+  res.send(html)
+}
+);
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
