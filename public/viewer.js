35ddc9a8ad3485171ea7f8c8f3ce274b30811482
antonioganea <antonio.ganea@yahoo.com>
2021-03-26 23:42:00 +0200

    Barilla object

--- public/viewer.js
+++ public/viewer.js
@@ -3,7 +3,11 @@ const createScene =  () => {
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
+    //const box = BABYLON.MeshBuilder.CreateBox("box", {});
+
+    BABYLON.SceneLoader.ImportMeshAsync("", "/", "barilla-optimized.glb"); //empty string all meshes
+
+
     return scene;
 }
 
