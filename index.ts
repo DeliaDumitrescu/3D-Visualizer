10e225e66d99d5a3e3238b0e37750cf49468e76d
antonioganea <antonio.ganea@yahoo.com>
2021-04-30 11:59:00 +0300

    Extra stuff

--- index.ts
+++ index.ts
@@ -2,11 +2,39 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = 80;
+
 app.use(express.static('public'))
+
+app.get('/watch', (req,res) => {
+  let videoID = req.query["v"]
+
+  let ejs = require('ejs');
+  let html = ejs.render('Salutare, vad ca vrei sa te uiti la <b>videoul</b> asta <%= videoID %> , se pare.', {videoID: videoID});
+
+  res.send(html)
+  
+})
+
+app.get('/register', (req,res) => {
+  res.send("Aici ar trebui sa te inregistrezi.")
+})
+
+app.get('/login', (req,res) => {
+  res.send("Aici ar trebui sa te loghezi.")
+})
+
+app.post('/logout', (req,res) => {
+  // ...
+})
+
+app.get('/wall/:user/', (req,res) => {
+  res.send("Esti pe wallul lui " + req.params["user"])
+})
+
 app.get('/', (req, res) =>{
 
   let ejs = require('ejs');
-  let people = ['geddy', 'neil', 'alex'];
+  let people = ['antonio', 'delia', 'liviu', 'silviu'];
   let html = ejs.render('<%= people.join(", "); %> </br> For more, click <a href="viewer.html">here</a>.', {people: people});
 
   res.send(html)
